5b12aa59c65b61e0fc125109f2f64d1717b4b5fa
brysongroves <grovesbryson54@gmail.com>
2025-05-09 00:59:39 -0400

    file name change... again

--- final-wumpus-unittests.py
+++ final-wumpus-unittests.py
@@ -7,7 +7,7 @@ import random
 from unittest.mock import patch
 
 # Import the move_wumpus function
-from 384-final.py import move_wumpus, cave
+from final-384.py import move_wumpus, cave
 
 class TestHuntTheWumpus(unittest.TestCase):
 
